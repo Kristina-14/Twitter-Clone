8b4948bbc880c412ce61a38626159f4d2d5f27bb
Kristina-14 <kristina.barooah@lpu.in>
2024-06-08 21:46:17 +0530

    Added the Sidebars and Body UI

--- twitter-clone-new/pages/_app.tsx
+++ twitter-clone-new/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
+//import Modal from '../components/layout/Modal'
 
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
 return(
+    <>
+    {/* <Modal actionLabel="Submit" isOpen title='Test Model'/> */}
     <Layout>
     <Component {...pageProps} />
     </Layout>
+    </>
 )
 }
